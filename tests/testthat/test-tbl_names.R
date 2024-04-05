86416832295bc61212b98772a5e7b79938c6e8eb
Charles Bailey <baileyc@chop.edu>
2024-04-04 22:39:23 -0400

    Tests for tbl_names.R

--- tests/testthat/test-tbl_names.R
+++ tests/testthat/test-tbl_names.R
@@ -1,23 +1,115 @@
 sess <- argos$new()
+# Insure known default state for functional testing
 set_argos_default(sess)
-con <- test_db_omop()
+con <- mk_testdb_omop()
+sess$config('db_src', con)
+sess$config('cdm_schema', 'test_cdm')
+sess$config('results_schema', 'test_result')
+sess$config('results_name_tag', '_test000')
+sess$config('local_name_tag', '_loc')
+sess$config('table_names', list('person' = 'personal'))
 
-test_that("Returns name if base name not found in `config('table_names') (method)", {
-  expect_equal(sess$qual_name('new_table', 'cdm_schema', con), 'new_table')
+test_that('qual_name passes through fully-qualified name', {
+  proband <- dbplyr::in_schema('known_schema', 'table')
+  expect_equal(sess$qual_name(proband, 'cdm_schema'),
+               proband)
 })
 
-test_that("Returns name if base name not found in `config('table_names') (function)", {
-  expect_equal(qual_name('new_table', 'cdm_schema', con), 'new_table')
+test_that("qual_name handles unmapped base name (method)", {
+  expect_equal(sess$qual_name('new_table', 'cdm_schema'),
+               dbplyr::in_schema('test_cdm', 'new_table'))
 })
 
-test_that("Returns temporary table name with results_tag (method)", {
-  expect_equal(sess$intermed_name('temp_tbl', TRUE, '_results_tag', NA, 'results_schema', con), 'temp_tbl_results_tag')
+test_that("qual_name handles unmapped base name (function)", {
+  expect_equal(qual_name('new_table', 'cdm_schema'),
+               dbplyr::in_schema('test_cdm', 'new_table'))
 })
 
-test_that("Returns temporary table name with results_tag (function)", {
-  expect_equal(intermed_name('temp_tbl', TRUE, '_results_tag', NA, 'results_schema', con), 'temp_tbl_results_tag')
+test_that("qual_name handles mapped base name  (method)", {
+  expect_equal(sess$qual_name('person', 'cdm_schema'),
+               dbplyr::in_schema('test_cdm', 'personal'))
 })
 
-test_that("Returns results table name with results_tag (function)", {
-  expect_equal(results_name('rslt_tbl', '_results_tag', NA, con), 'rslt_tbl_results_tag')
+test_that("qual_name handles mapped base name  (function)", {
+  expect_equal(qual_name('person', 'cdm_schema'),
+               dbplyr::in_schema('test_cdm', 'personal'))
+})
+
+test_that("intermed_name returns temporary table name with results_tag (method)", {
+  expect_equal(sess$intermed_name('temp_tbl', temporary = TRUE,
+                                  results_tag = '_results_tag',
+                                  local_tag = NA,
+                                  schema = 'results_schema'),
+               'temp_tbl_results_tag')
+})
+
+test_that("intermed_name returns temporary table name with results_tag (function)", {
+  expect_equal(intermed_name('temp_tbl', temporary = TRUE,
+                             results_tag = '_results_tag',
+                             local_tag = NA,
+                             schema = 'results_schema'),
+               'temp_tbl_results_tag')
+})
+
+test_that("intermed_name returns qualified table name with results_tag (method)", {
+  expect_equal(sess$intermed_name('perm_tbl', temporary = FALSE,
+                                  local_tag = NA,
+                                  schema = 'results_schema'),
+               dbplyr::in_schema('test_result',
+                                 'perm_tbl_test000'))
+})
+
+test_that("intermed_name returns qualified table name with results_tag (function)", {
+  expect_equal(intermed_name('perm_tbl', temporary = FALSE,
+                             local_tag = NA,
+                             schema = 'results_schema'),
+               dbplyr::in_schema('test_result',
+                                 'perm_tbl_test000'))
+})
+
+test_that("intermed_name returns qualified table name with local_tag (method)", {
+  expect_equal(sess$intermed_name('perm_tbl', temporary = FALSE,
+                                  local_tag = TRUE,
+                                  schema = 'results_schema'),
+               dbplyr::in_schema('test_result',
+                                 'perm_tbl_loc_test000'))
+})
+
+test_that("intermed_name returns qualified table name with local_tag (function)", {
+  expect_equal(intermed_name('perm_tbl', temporary = FALSE,
+                             local_tag = TRUE,
+                             schema = 'results_schema'),
+               dbplyr::in_schema('test_result',
+                                 'perm_tbl_loc_test000'))
+})
+
+test_that('intermed_name adds tags to fully-qualified name', {
+  expect_equal(sess$intermed_name(dbplyr::in_schema('known', 'table'),
+                                  temporary = FALSE, local_tag = TRUE,
+                                  schema = 'results_schema'),
+               dbplyr::in_schema('known', 'table_loc_test000'))
+})
+
+test_that("results_name returns qualified table name with results_tag (method)", {
+  expect_equal(sess$results_name('perm_tbl', local_tag = NA),
+               dbplyr::in_schema('test_result',
+                                 'perm_tbl_test000'))
+})
+
+test_that("results_name returns qualified table name with results_tag (function)", {
+  expect_equal(results_name('perm_tbl', local_tag = NA),
+               dbplyr::in_schema('test_result',
+                                 'perm_tbl_test000'))
+})
+
+test_that("results_name returns qualified table name with local_tag (method)", {
+  expect_equal(sess$results_name('perm_tbl', local_tag = TRUE),
+               dbplyr::in_schema('test_result',
+                                 'perm_tbl_loc_test000'))
+})
+
+test_that("results_name returns qualified table name with local_tag (function)", {
+  expect_equal(results_name('perm_tbl', local_tag = TRUE),
+               dbplyr::in_schema('test_result',
+                                 'perm_tbl_loc_test000'))
 })
